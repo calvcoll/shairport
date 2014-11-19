48904969eb77eef4cb7b040146440ab736cf6058
Calv Collins <calvcoll@gmail.com>
2014-11-19 22:31:34 +0000

    Compiles in with GTK

--- app_indicator.c
+++ app_indicator.c
@@ -1,6 +1,28 @@
 #include <gtk/gtk.h>
 #include <libappindicator/app-indicator.h>
-#include "app.h"
+#include "app_indicator.h"
+
+static GtkActionEntry entries[] = {
+  { "New",      "document-new", "_New", "<control>N",
+    "Create a new file", G_CALLBACK (activate_action) },
+  { "Open",     "document-open", "_Open", "<control>O",
+    "Open a file", G_CALLBACK (activate_action) },
+  { "Save",     "document-save", "_Save", "<control>S",
+    "Save file", G_CALLBACK (activate_action) },
+  { "Quit",     "application-exit", "_Quit", "<control>Q",
+    "Exit the application", G_CALLBACK (gtk_main_quit) },
+};
+static guint n_entries = G_N_ELEMENTS (entries);
+
+static const gchar *ui_info =
+"<ui>"
+"  <popup name='IndicatorPopup'>"
+"    <menuitem action='New' />"
+"    <menuitem action='Open' />"
+"    <menuitem action='Save' />"
+"    <menuitem action='Quit' />"
+"  </popup>"
+"</ui>";
 
 static void
 activate_action (GtkAction *action)
