0dfd365fab9395ab6d93c7ae543b7cf85ef875d5
Calv Collins <calvcoll@gmail.com>
2014-11-19 21:47:34 +0000

    Added app-indicator files - not finished at all.

---
+++ app-indicator.c
@@ -0,0 +1,68 @@
+#include <gtk/gtk.h>
+#include <libappindicator/app-indicator.h>
+#include "app.h"
+
+static void
+activate_action (GtkAction *action)
+{
+        const gchar *name = gtk_action_get_name (action);
+        GtkWidget *dialog;
+
+        dialog = gtk_message_dialog_new (NULL,
+                                         GTK_DIALOG_DESTROY_WITH_PARENT,
+                                         GTK_MESSAGE_INFO,
+                                         GTK_BUTTONS_CLOSE,
+                                         "You activated action: \"%s\"",
+                                         name);
+
+        g_signal_connect (dialog, "response",
+                          G_CALLBACK (gtk_widget_destroy), NULL);
+
+        gtk_widget_show (dialog);
+}
+
+int main (int argc, char **argv)
+{
+  GObject *dead;
+  GtkWidget *indicator_menu;
+  GtkActionGroup *action_group;
+  GtkUIManager *uim;
+  AppIndicator *indicator;
+  GError *error = NULL;
+
+  gtk_init (&argc, &argv);
+
+  action_group = gtk_action_group_new ("AppActions");
+  gtk_action_group_add_actions (action_group,
+                                entries, n_entries,
+                                dead);
+
+  /* Menus */
+  uim = gtk_ui_manager_new ();
+
+  gtk_ui_manager_insert_action_group (uim, action_group, 0);
+
+  if (!gtk_ui_manager_add_ui_from_string (uim, ui_info, -1, &error))
+    {
+      g_message ("Failed to build menus: %s\n", error->message);
+      g_error_free (error);
+      error = NULL;
+    }
+
+  /* Indicator */
+  indicator = app_indicator_new ("test-app",
+                                 "/home/calv/tmp/test/ice.png",
+                                 APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
+
+  indicator_menu = gtk_ui_manager_get_widget (uim, "/ui/IndicatorPopup");
+
+  app_indicator_set_icon_theme_path(indicator, "/home/calv/tmp/test/");
+  app_indicator_set_status (indicator, APP_INDICATOR_STATUS_ACTIVE);
+  app_indicator_set_attention_icon (indicator, "indicator-messages-new");
+
+  app_indicator_set_menu (indicator, GTK_MENU (indicator_menu));
+
+  gtk_main ();
+
+  return 0;
+}
